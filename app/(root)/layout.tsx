ad0b2f19cdb9dffc96ca76f681f16b08dacab008
jarutosurano <jarutosurano@gmail.com>
2024-08-19 21:35:19 -0400

    implement header section and sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Rusty', lastName: 'Chase'}
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
         {children}
     </main>
   );
---
+++ components/Sidebar.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import { usePathname } from 'next/navigation'
+import path from 'path'
+
+
+const Sidebar = ({user}: SiderbarProps) => {
+    const pathname = usePathname();
+    
+    return (
+        <section className='sidebar'>
+            <nav className='flex flex-col gap-4'>
+                <Link href='/' className='mb-12 cursor-pointer items-center gap-2'>
+                    <Image 
+                        src='/icons/logo.svg'
+                        width={34}
+                        height={34}
+                        alt="Horizon logo"
+                        className="size-[24px] max-xl:size-14"
+                    />
+                    <h1 className='sidebar-logo'>Horizon</h1>
+                </Link>
+                {sidebarLinks.map((item) => {
+                    const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`);
+
+                    return (
+                        <Link href={item.route} key={item.label} className={cn('sidebar-link', { 'bg-bank-gradient': isActive})}>
+                            <div className='relative size-6'>
+                                <Image 
+                                
+                                />
+                            </div>
+                            {item.label}
+                        </Link>
+                    )
+                })}
+            </nav>
+        </section>
+    )
+}
+
+export default Sidebar
